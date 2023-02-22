ed4f85b18644a81f3298f82af7abf4fe0e9b7fed
Augusto <auguzsto@gmail.com>
2023-02-22 14:27:02 -0300

    singleChildScrollView + fprmat date

--- lib/src/services/utils_services.dart
+++ lib/src/services/utils_services.dart
@@ -1,5 +1,18 @@
+import 'package:intl/intl.dart';
+
 class UtilServices {
   String replaceName(String? name) {
     return name!.replaceAll("\$#\$", "").substring(0, 13).trimLeft();
+  }
+
+  String replacePrice(double? price) {
+    return price!.toString().replaceAll(".", ",");
+  }
+
+  String replaceUpdate(String? date) {
+    DateFormat format = DateFormat('dd/MM/yyyy HH:mm');
+
+    return format
+        .format(DateTime.parse(date!.replaceAll("T", " ").substring(0, 16)));
   }
 }
